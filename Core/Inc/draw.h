155de49e69d42c55f7cd3282fc6f2f3956a2d5ca
Maïssa Elafani <166125975+maissaelafani@users.noreply.github.com>
2024-04-12 13:02:01 +0200

    Add files via upload

---
+++ Core/Inc/draw.h
@@ -0,0 +1,26 @@
+/*
+ * draw.h
+ *
+ *  Created on: Mar 20, 2024
+ *      Author: maiss
+ */
+
+#ifndef INC_DRAW_H_
+#define INC_DRAW_H_
+
+#include <_ansi.h>
+
+_BEGIN_STD_C
+
+void ssd1306_DrawLogo();
+void ssd1306_DrawLogoBis();
+
+_END_STD_C
+
+
+#endif /* INC_DRAW_H_ */
+
+
+
+
+
